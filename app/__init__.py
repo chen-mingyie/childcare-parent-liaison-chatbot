fb923db348d372eeb5a4c0ef88f1f211f1a37b11
cmingyi <chen_mingyie@yahoo.com>
2020-11-24 00:48:43 +0800

    Setup for deployment into heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 import os
 
 # ---------- Uncomment below if you wish to start chatbot backend in localhost
-from app.model.secret_idandkeys import SecretData as s
-os.environ["GOOGLE_CREDENTIALS"] = s.getSecrets('GOOGLE_CREDENTIALS') # contents of json key
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = s.getSecrets('GOOGLE_APPLICATION_CREDENTIALS') # json key for google project link to dialogflow
-os.environ['PROJECT_ID'] = s.getSecrets('PROJECT_ID')# google project id
-os.environ['TELEGRAM_TOKEN'] = s.getSecrets('TELEGRAM_TOKEN') # token for telegram bot
+# from app.model.secret_idandkeys import SecretData as s
+# os.environ["GOOGLE_CREDENTIALS"] = s.getSecrets('GOOGLE_CREDENTIALS') # contents of json key
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = s.getSecrets('GOOGLE_APPLICATION_CREDENTIALS') # json key for google project link to dialogflow
+# os.environ['PROJECT_ID'] = s.getSecrets('PROJECT_ID')# google project id
+# os.environ['TELEGRAM_TOKEN'] = s.getSecrets('TELEGRAM_TOKEN') # token for telegram bot
 # ----------
 
 here = os.path.dirname(os.path.abspath(__file__))
